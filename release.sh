d995e8649555a00840f113a70eed12691e038a7f
Michael Vittrup Larsen <mvl.gh@network42.dk>
2025-02-02 10:59:44 +0100

    test release

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 GITHUB_SHA=$1
-IMAGE=$(ko build --base-import-paths --platform=all -t latest -t "sha-$GITHUB_SHA" ./)
+IMAGE=$(ko build --base-import-paths --platform="linux/arm64,linux/amd64" -t latest -t "sha-$GITHUB_SHA" ./)
 echo "built $IMAGE"
 cosign sign "$IMAGE" --yes -a sha="sha-$GITHUB_SHA"
